c6bd5f995b75de1172549bf88e837672a4f8cd7a
Can Ersoz <canersoz@berkeley.edu>
2021-02-28 19:03:01 +0300

    changed textfield dynamics

--- Sources/PTUIKit/TextFields/TextImageInputView.swift
+++ Sources/PTUIKit/TextFields/TextImageInputView.swift
@@ -8,43 +8,74 @@
 import SwiftUI
 
 struct TextImageInputView: View {
-    @State var imageSystemName: String
-    @State var headline: String
-    @State var textFieldPrompt: String
-    @State var secure: Bool
     @Binding var inputText: String
     
+    let image: Image?
+    let headline: String
+    let textFieldPrompt: String
+    let secure: Bool
+    let inputType: UITextContentType?
+    let color: Color
+    let textColor: Color
+    let headlineColor: Color
+    
+    init(headline: String, textFieldPrompt: String, inputText: Binding<String>, image: Image? = nil, inputType: UITextContentType? = nil, secure: Bool = false, color: Color = .black, textColor: Color = .black, headlineColor: Color = .gray) {
+        self.image = image
+        self.headline = headline
+        self.textFieldPrompt = textFieldPrompt
+        self._inputText = inputText
+        self.inputType = inputType
+        self.secure = secure
+        self.color = color
+        self.textColor = textColor
+        self.headlineColor = headlineColor
+    }
+    
+    @ViewBuilder
+    var field: some View {
+        if secure {
+            SecureField(textFieldPrompt, text: $inputText)
+        }
+        else {
+            TextField(textFieldPrompt, text: $inputText)
+        }
+    }
+    
+    @ViewBuilder
+    var contentTypedField: some View {
+        if let t = inputType { field.textContentType(t) }
+        else { field }
+    }
+    
     var body: some View {
-        VStack(alignment: .leading) {
+        VStack(alignment: .leading, spacing: 0) {
             Text(headline)
                 .font(.headline)
-                .foregroundColor(Color(.darkGray))
-            
+                .foregroundColor(headlineColor)
             HStack {
-                if secure {
-                    SecureField(textFieldPrompt, text: $inputText)
-                        .textContentType(.emailAddress)
-                        .padding(.top, 15)
-                        .padding(.bottom, 10)
-                        .font(.headline)
+                contentTypedField
+                    .foregroundColor(textColor)
+                    .padding(.top, 10)
+                    .padding(.bottom, 5)
+                    .font(.headline)
+                if let image = image {
+                    image
+                        .renderingMode(.template)
+                        .foregroundColor(color)
+                        .padding(.trailing)
                 }
-                else {
-                    TextField(textFieldPrompt, text: $inputText)
-                        .textContentType(.emailAddress)
-                        .padding(.top, 15)
-                        .padding(.bottom, 10)
-                        .font(.headline)
-                }
-                Image(systemName: imageSystemName).padding(.trailing)
-            }
+            }.padding(0)
             
-            Divider().background(Color.black)
+            Divider().background(color)
         }
     }
 }
 
 struct TextImageInputView_Previews: PreviewProvider {
     static var previews: some View {
-        TextImageInputView(imageSystemName: "lock.fill", headline: "Password", textFieldPrompt: "Password", secure: true, inputText: .constant("Hey"))
+        VStack{
+            TextImageInputView(headline: "Password", textFieldPrompt: "Password", inputText: .constant("Hegdfhjy"), image: Image(systemName: "lock.fill"), inputType: .password, secure: false, color: .blue, textColor: .green, headlineColor: .pink)
+            TextImageInputView(headline: "Password", textFieldPrompt: "Password", inputText: .constant("Hey"))
+        }
     }
 }
